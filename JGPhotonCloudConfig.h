7efd37b2730e4e5aeb81e10328bb807e707f5cc4
alwaysgreenleaf <kimbobv22@gmail.com>
2013-02-14 15:01:56 +0900

    fix config

--- JGPhotonCloudConfig.h
+++ JGPhotonCloudConfig.h
@@ -6,7 +6,7 @@
  
 */
 
-#define jgVarJGPhotonCloudConfig_appID @"29d58d17-cabb-4ab5-891a-b3cf84782d5f"
+#define jgVarJGPhotonCloudConfig_appID @"Add your AppId"
 #define jgVarJGPhotonCloudConfig_version @"v1.0"
 #define jgVarJGPhotonCloudConfig_usingTcp true
 #define jgVarJGPhotonCloudConfig_debugLevel DEBUG_LEVEL_OFF
